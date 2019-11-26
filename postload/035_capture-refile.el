52c97d843c17e26ec3595bcdd0a151b8832c875d
Iannis Zannos <zannos@gmail.com>
2019-11-26 10:09:12 +0200

    refile hydra: switch agenda files

--- postload/035_capture-refile.el
+++ postload/035_capture-refile.el
@@ -8,9 +8,38 @@
         ("j" "Journal" entry (file+datetree org-default-notes-file)
          "* %?\nEntered on %U\n  %i\n  %a")))
 
-(setq org-refile-targets
-  '(("~/Documents/000Workfiles/PROJECTS_CURRENT/PROJECT_SUMMARIES.org"
-     :maxlevel . 3)))
+(defun org-refile-set-target-level-1 ()
+  "Set maxlevel 1"
+  (interactive)
+  (setq org-refile-targets
+        '(("~/Documents/000Workfiles/PROJECTS_CURRENT/PROJECT_SUMMARIES.org"
+           :maxlevel . 1))))
+
+(defun org-refile-set-target-level-2 ()
+  "Set maxlevel 2"
+  (interactive)
+  (setq org-refile-targets
+        '(("~/Documents/000Workfiles/PROJECTS_CURRENT/PROJECT_SUMMARIES.org"
+           :maxlevel . 2))))
+
+(defun org-refile-set-target-level-3 ()
+  "Set maxlevel 3"
+  (interactive)
+  (setq org-refile-targets
+        '(("~/Documents/000Workfiles/PROJECTS_CURRENT/PROJECT_SUMMARIES.org"
+           :maxlevel . 3))))
+
+(defun org-set-agenda-files-global ()
+  "Set org-agenda-files to global file paths."
+  (interactive)
+  (setq org-agenda-files `(,org-journal-dir
+                           ,org-projects-dir
+                           ,org-avarts-dir)))
+
+(defun org-set-agenda-files-project ()
+  "Set org-agenda-files to project summaries."
+  (interactive)
+  (setq org-agenda-files '("~/Documents/000Workfiles/PROJECTS_CURRENT/PROJECT_SUMMARIES.org")))
 
 (global-set-key (kbd "C-S-c C-S-c") 'org-capture)
 (global-set-key (kbd "C-S-c C-S-r") 'org-capture-goto-last-stored)
@@ -36,6 +65,11 @@
   ("R" org-refile "refile (do not keep)")
   ("j" org-refile-jump-to-target "jump to refile target")
   ("l" org-refile-goto-last-stored "goto last stored")
+  ("p" org-set-agenda-files-project "agenda use project file")
+  ("g" org-set-agenda-files-global "agenda use global files")
+  ("1" org-refile-set-target-level-1 "show only level 1")
+  ("2" org-refile-set-target-level-2 "show down to level 2")
+  ("3" org-refile-set-target-level-3 "show down to level 3")
   ;; ("q" quit "quit" :exit t)
   )
 
